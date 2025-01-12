f7f0d3280c58260369f88a7c4d4b1c6631d40be9
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-01-12 15:07:43 +0100

    step 6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,15 @@
-export const countContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8'); // Зчитуємо вміст файлу db.json
+    const allContacts = JSON.parse(data); // Перетворюємо JSON-рядок у масив об'єктів
+    return allContacts.length; // Повертаємо масив контактів
+  } catch (error) {
+    console.error('Error reading contacts:', error.message);
+    throw new Error('Failed to get contacts');
+  }
+};
 
 console.log(await countContacts());

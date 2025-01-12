1defd6525ae4909dcaae27e58fea9614082091bd
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-01-12 15:44:36 +0100

    step 7

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,19 @@
-export const removeAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+
+export const removeAllContacts = async () => {
+  try {
+    const emptyContacts = []; // Очищаємо масив контактів
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(emptyContacts, null, 2),
+      'utf-8',
+    ); // // Записуємо порожній масив у файл
+    console.log('All contacts have been removed successfully.');
+  } catch (error) {
+    console.error('Error removing contacts:', error.message);
+    throw new Error('Failed to remove all contacts');
+  }
+};
 
 removeAllContacts();
